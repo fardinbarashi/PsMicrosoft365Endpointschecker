4ba40d17008a2757d0617ae6d8fd25267ba2e463
Code Ape <fardin.barashi@gmail.com>
2023-12-20 21:54:29 +0100

    Update Microsoft 365 Endpoints.ps1

--- Microsoft 365 Endpoints.ps1
+++ Microsoft 365 Endpoints.ps1
@@ -1,15 +1,14 @@
 <#
 System requirements
-PSVersion: 5.1.19041.3803
-PSEdition: Desktop
-PSCompatibleVersions: 1.0, 2.0, 3.0, 4.0, 5.0, 5.1.19041.3803
-BuildVersion: 10.0.19041.3803
-CLRVersion: 4.0.30319.42000
-WSManStackVersion: 3.0
-PSRemotingProtocolVersion: 2.3
-SerializationVersion: 1.1.0.1
+PSVersion                      7.3.1
+PSEdition                      Core
+GitCommitId                    7.3.1
+PSCompatibleVersions           {1.0, 2.0, 3.0, 4.0…}
+PSRemotingProtocolVersion      2.3
+SerializationVersion           1.1.0.1
+WSManStackVersion              3.0  
 
-About Script : 
+About Script : Template for Ps-7 Scripts
 Author : Fardin Barashi
 Title : Microsoft 365 Endpoints checker
 Description : The Office 365 IP Address and URL web service assists users in better identifying and distinguishing Office 365 network traffic, making it easier to assess, configure, and stay updated on changes. This REST-based web service replaces the previously downloadable XML files, which were phased out on October 2, 2018  This script checks after which ip-adresses that M365 Endpoints is needed. 
@@ -42,13 +41,12 @@ $uri = "https://endpoints.office.com/endpoints/worldwide?NoIPv6=true&clientReque
 
 #----------------------------------- Start Script ------------------------------------------
 
-
 # Section 1 : Connect to Uri
-$Section = "Section 1 : Connect to $uri"
+$Section1 = "Section 1 : Connect to $uri"
 Try
 { # Start Try, $Section
  Get-Date -Format "yyyy/MM/dd HH:mm:ss"
- Write-Host $Section... "0%" -ForegroundColor Yellow
+ Write-Host $Section1... "0%" -ForegroundColor Yellow
  $EndpointSets = Invoke-RestMethod -Uri ($uri) -Verbose
          If ( $EndpointSets -Eq $Null  ) 
           { # Start If, $EndpointSets is empty or null
@@ -64,55 +62,65 @@ Try
           { # Start Else, $EndpointSets is Not empty or null
             Write-host ""
             Get-Date -Format "yyyy/MM/dd HH:mm:ss"
-            Write-Host $Section... "100%" -ForegroundColor Green
+            Write-host "Connect to $Uri is successful"
 
-            # Optimize
-            Write-host ""
-            Write-host "-----------"
-            Write-host ""
-            Write-Host "Getting all the Optimize Endpoints" 
-            $Optimize = $endpointSets | Where-Object { $_.category -eq "Optimize" }
-            $OptimizeIpsv4 = $Optimize.ips | Where-Object { ($_).contains(".") } | Sort-Object -Unique     
-            Write-Host $OptimizeIpsv4
-
-            Write-host ""
-            Write-Host "Saving all the Optimize Endpoints to a textfile" 
-            $optimizeIpsv4 | out-file -FilePath "$PSScriptRoot\Files\Microsoft Optimize Endpoints $LogFileDate.Txt" -Verbose -Encoding utf8 -Force
-
-            # Allow
-            Write-host ""
-            Write-host "-----------"
-            Write-host ""
-            Write-Host "Getting all the Allow Endpoints" 
-            $Allow = $endpointSets | Where-Object { $_.category -eq "Allow" }
-            $AllowIpsv4 = $Allow.ips | Where-Object { ($_).contains(".") } | Sort-Object -Unique
-            Write-Host $AllowIpsv4
-
-            Write-host ""
-            Write-Host "Saving all the Allow Endpoints to a textfile"            
-            $AllowIpsv4 | out-file -FilePath "$PSScriptRoot\Files\Microsoft Allow Endpoints $LogFileDate.Txt" -Verbose -Encoding utf8 -Force
-
-            # Default
-            Write-host ""
-            Write-host "-----------"
-            Write-host ""
-            Write-Host "Getting all the Default Endpoints" 
-            $Default = $endpointSets | Where-Object { $_.category -eq "Default" }
-            $DefaultIpsv4 = $Allow.ips | Where-Object { ($_).contains(".") } | Sort-Object -Unique
-            Write-Host $DefaultIpsv4
-            
-            Write-host ""
-            Write-Host "Saving all the Default Endpoints to a textfile"            
-            $DefaultIpsv4 | out-file -FilePath "$PSScriptRoot\Files\Microsoft Default Endpoints $LogFileDate.Txt" -Verbose -Encoding utf8 -Force
-
-            Write-host ""
-            Write-host "-----------"
 
           } # End Else, $EndpointSets is Not empty or null
+} # End Try
 
- # Run Query
-
+Catch
+{ # Start Catch
+Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+ Write-Host "ERROR on $Section1" -ForegroundColor Red
+ # Get-Errors
+ Write-Warning $Error[0]
+ Write-Warning $Error[0].CategoryInfo
  Write-Host ""
+ Write-Warning $Error[0].InvocationInfo
+ Write-Host ""
+ Write-Warning $Error[0].Exception
+ Write-Host ""
+ Write-Warning $Error[0].FullyQualifiedErrorId
+ Write-Host ""
+ Write-Warning $Error[0].PipelineIterationInfo
+ Write-Host ""
+ Write-Warning $Error[0].ScriptStackTrace
+ Write-Host ""
+ Write-Warning $Error[0].TargetObject
+ Write-Host ""
+ Write-Warning $Error[0].PSMessageDetails
+ Write-Host "Stopping Transcript and Script!" -ForegroundColor Red
+ Stop-Transcript
+ Exit
+} # End Catch
+
+# Section 2 : Get all the Getting all the Optimize Endpoints and save as a textfile
+$Section2 = "Section 2:  Get all the Getting all the Optimize Endpoints and save as a textfile"
+Try
+{ # Start Try, $Section
+  Write-Host $Section1... "100%" -ForegroundColor Green
+  Write-host ""
+  
+  Write-host ""
+  Write-host "-----------"
+  Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+  Write-Host $Section2... "0%" -ForegroundColor Yellow
+  
+
+
+  Write-host ""
+  Write-Host "Getting all the Optimize Endpoints" 
+  $Optimize = $endpointSets | Where-Object { $_.category -eq "Optimize" }
+  $OptimizeIpsv4 = $Optimize.ips | Where-Object { ($_).contains(".") } | Sort-Object -Unique     
+  $Optimizedata  = $Optimize 
+  Write-Host $OptimizeIpsv4
+
+  Write-host ""
+  Write-Host "Saving all the Optimize Endpoints to a textfile" 
+  $Optimizedata | out-file -FilePath "$PSScriptRoot\Files\Microsoft Optimize Endpoints data $LogFileDate.Txt" -Verbose -Encoding utf8 -Force   
+  $OptimizeIpsv4 | out-file -FilePath "$PSScriptRoot\Files\Microsoft Optimize Endpoints $LogFileDate.Txt" -Verbose -Encoding utf8 -Force
+
+  Write-Host ""
 } # End Try
 
 Catch
@@ -120,7 +128,119 @@ Catch
 Get-Date -Format "yyyy/MM/dd HH:mm:ss"
  Write-Host "ERROR on $Section" -ForegroundColor Red
  # Get-Errors
- Get-Error
+ Write-Warning $Error[0]
+ Write-Warning $Error[0].CategoryInfo
+ Write-Host ""
+ Write-Warning $Error[0].InvocationInfo
+ Write-Host ""
+ Write-Warning $Error[0].Exception
+ Write-Host ""
+ Write-Warning $Error[0].FullyQualifiedErrorId
+ Write-Host ""
+ Write-Warning $Error[0].PipelineIterationInfo
+ Write-Host ""
+ Write-Warning $Error[0].ScriptStackTrace
+ Write-Host ""
+ Write-Warning $Error[0].TargetObject
+ Write-Host ""
+ Write-Warning $Error[0].PSMessageDetails
+ Write-Host "Stopping Transcript and Script!" -ForegroundColor Red
+ Stop-Transcript
+ Exit
+} # End Catch
+
+
+# Section 3 : Get all the Getting all the Allow Endpoints and save as a textfile
+$Section3 = "Section 3 :  Get all the Getting all the Allow Endpoints and save as a textfile"
+Try
+{ # Start Try, $Section
+ Write-Host $Section2... "100%" -ForegroundColor Green
+ Write-host ""
+  
+ Write-host ""
+ Write-host "-----------"
+
+ Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+ Write-Host $Section3... "0%" -ForegroundColor Yellow
+
+ Write-host ""
+ Write-Host "Getting all the Allow Endpoints" 
+ $Allow = $endpointSets | Where-Object { $_.category -eq "Allow" }
+ $AllowIpsv4 = $Allow.ips | Where-Object { ($_).contains(".") } | Sort-Object -Unique
+ $Allowdata  = $Allow
+ Write-Host $AllowIpsv4
+
+ Write-host ""
+ Write-Host "Saving all the Allow Endpoints to a textfiles"  
+ $Allowdata | out-file -FilePath "$PSScriptRoot\Files\Microsoft Allow Endpoints data $LogFileDate.Txt" -Verbose -Encoding utf8 -Force          
+ $AllowIpsv4 | out-file -FilePath "$PSScriptRoot\Files\Microsoft Allow Endpoints Ipsv4 $LogFileDate.Txt" -Verbose -Encoding utf8 -Force
+
+ Write-Host ""
+} # End Try
+
+
+Catch
+{ # Start Catch
+Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+ Write-Host "ERROR on $Section" -ForegroundColor Red
+ # Get-Errors
+ Write-Warning $Error[0]
+ Write-Warning $Error[0].CategoryInfo
+ Write-Host ""
+ Write-Warning $Error[0].InvocationInfo
+ Write-Host ""
+ Write-Warning $Error[0].Exception
+ Write-Host ""
+ Write-Warning $Error[0].FullyQualifiedErrorId
+ Write-Host ""
+ Write-Warning $Error[0].PipelineIterationInfo
+ Write-Host ""
+ Write-Warning $Error[0].ScriptStackTrace
+ Write-Host ""
+ Write-Warning $Error[0].TargetObject
+ Write-Host ""
+ Write-Warning $Error[0].PSMessageDetails
+ Write-Host "Stopping Transcript and Script!" -ForegroundColor Red
+ Stop-Transcript
+ Exit
+} # End Catch
+
+
+# Section 4 : Get all the Getting all the Default Endpoints and save as a textfile
+$Section4 = "Section 4 :  Get all the Getting all the Default Endpoints and save as a textfile"
+Try
+{ # Start Try, $Section
+ Write-Host $Section3... "100%" -ForegroundColor Green
+ Write-host ""
+  
+ Write-host ""
+ Write-host "-----------"
+
+ Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+ Write-Host $Section4... "0%" -ForegroundColor Yellow
+
+
+ Write-host ""
+ Write-Host "Getting all the Default Endpoints" 
+ $Default = $endpointSets | Where-Object { $_.category -eq "Default" }
+ $Defaultdata  = $Default
+ $DefaultIpsv4 = $Default.urls | Where-Object { ($_).contains(".") } | Sort-Object -Unique
+ Write-Host $DefaultIpsv4
+            
+ Write-host ""
+ Write-Host "Saving all the Default Endpoints to a textfiles"  
+ $Defaultdata | out-file -FilePath "$PSScriptRoot\Files\Microsoft Default data Endpoints $LogFileDate.Txt" -Verbose -Encoding utf8 -Force          
+ $DefaultIpsv4 | out-file -FilePath "$PSScriptRoot\Files\Microsoft Default Endpoints $LogFileDate.Txt" -Verbose -Encoding utf8 -Force
+
+ Write-Host ""
+} # End Try
+
+
+Catch
+{ # Start Catch
+Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+ Write-Host "ERROR on $Section" -ForegroundColor Red
+ # Get-Errors
  Write-Warning $Error[0]
  Write-Warning $Error[0].CategoryInfo
  Write-Host ""
@@ -144,3 +264,5 @@ Get-Date -Format "yyyy/MM/dd HH:mm:ss"
 
 #----------------------------------- End Script ------------------------------------------
 Stop-Transcript
+
+
